8e6f77fc33b9e73a44d6d42b2e601c02895fac00
Fl0Xxx <sonicsleepy@gmail.com>
2025-04-01 15:09:05 +0300

    created "Game" model

---
+++ app/Models/Game.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'developer',
+        'genre',
+        'release_date',
+        'platform',
+        'price',
+    ];
+}
