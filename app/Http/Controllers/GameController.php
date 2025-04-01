2f3ac1ae2b1e40b997372fa585a5b8b75d66896c
Fl0Xxx <sonicsleepy@gmail.com>
2025-04-01 15:09:58 +0300

    Implemented "Game" controller

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+        $games = Game::all();
+        return view('games.index', compact('games'));
+    }
+
+    public function create()
+    {
+        return view('games.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'title' => 'required',
+            'developer' => 'required',
+            'genre' => 'required',
+            'release_date' => 'required|date',
+            'platform' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        Game::create($request->all());
+
+        return redirect()->route('games.index');
+    }
+
+    public function edit(Game $game)
+    {
+        return view('games.edit', compact('game'));
+    }
+
+    public function update(Request $request, Game $game): RedirectResponse
+    {
+        $request->validate([
+            'title' => 'required',
+            'developer' => 'required',
+            'genre' => 'required',
+            'release_date' => 'required|date',
+            'platform' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        $game->update($request->all());
+
+        return redirect()->route('games.index');
+    }
+
+    public function destroy(Game $game): RedirectResponse
+    {
+        $game->delete();
+
+        return redirect()->route('games.index');
+    }
+}
