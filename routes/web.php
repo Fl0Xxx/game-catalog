52fdae98d5cf71dba6580987b04caf4992720baa
Fl0Xxx <sonicsleepy@gmail.com>
2025-04-02 13:21:39 +0300

    Added route for single page of game

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::resource('games', GameController::class);
+Route::get('/games/{game}', [GameController::class, 'show'])->name('games.show');
